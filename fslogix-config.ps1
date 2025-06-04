cca125ddd8301b086217c9f4631b317497460d80
saadrkhan <saadrahimkhan1@gmail.com>
2025-06-04 13:03:33 +0400

    Update fslogix-config.ps1

--- fslogix-config.ps1
+++ fslogix-config.ps1
@@ -1,27 +1,12 @@
-# Download and install FSLogix
-Invoke-WebRequest -Uri $fileUris[0] -OutFile "C:\Temp\FSLogix.zip"
-Expand-Archive -Path "C:\Temp\FSLogix.zip" -DestinationPath "C:\Temp\FSLogix"
-Start-Process -FilePath "C:\Temp\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet /norestart" -Wait
+# Ensure registry path exists
+New-Item -Path "HKLM:\SOFTWARE\FSLogix" -Name "Profiles" -Force | Out-Null
 
-# Configure FSLogix registry settings
+# Configure FSLogix
 $regPath = "HKLM:\SOFTWARE\FSLogix\Profiles"
-if (!(Test-Path $regPath)) {
-    New-Item -Path $regPath -Force
-}
+$sharePath = "\\${env:STORAGE_ACCOUNT}.file.core.windows.net\${env:SHARE_NAME}"
 
-# Set FSLogix configuration
 Set-ItemProperty -Path $regPath -Name "Enabled" -Value 1 -Type DWord
-Set-ItemProperty -Path $regPath -Name "VHDLocations" -Value "\\$($env:storageAccountName).file.core.windows.net\$($env:fileShareName)" -Type String
+Set-ItemProperty -Path $regPath -Name "VHDLocations" -Value $sharePath -Type MultiString
 Set-ItemProperty -Path $regPath -Name "DeleteLocalProfileWhenVHDShouldApply" -Value 1 -Type DWord
-Set-ItemProperty -Path $regPath -Name "FlipFlopProfileDirectoryName" -Value 1 -Type DWord
-Set-ItemProperty -Path $regPath -Name "IsDynamic" -Value 1 -Type DWord
+Set-ItemProperty -Path $regPath -Name "SizeInMBs" -Value 32768 -Type DWord
 Set-ItemProperty -Path $regPath -Name "VolumeType" -Value "VHDX" -Type String
-Set-ItemProperty -Path $regPath -Name "SizeInMBs" -Value 30000 -Type DWord
-
-# Enable Office containers if needed
-$officeRegPath = "HKLM:\SOFTWARE\Policies\FSLogix\ODFC"
-if (!(Test-Path $officeRegPath)) {
-    New-Item -Path $officeRegPath -Force
-}
-Set-ItemProperty -Path $officeRegPath -Name "Enabled" -Value 1 -Type DWord
-Set-ItemProperty -Path $officeRegPath -Name "VHDLocations" -Value "\\$($env:storageAccountName).file.core.windows.net\$($env:fileShareName)" -Type String
